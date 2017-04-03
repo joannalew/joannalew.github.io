f7564d0a3138c9c9bfd1c73b53f3766931a725f6
joannalew <Eggo@Joannas-MBP.lan>
2017-04-03 16:03:21 -0700

    epic file pathing / content update

--- js/scripts.js
+++ js/scripts.js
@@ -176,25 +176,6 @@ jQuery(document).ready(function($) {
 		}
 	});
 
-	// Custom Filter (Changed)
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
-        }
-    });
-
 
 	// Shop Filters Toggle
 	//------------------------------------------------------------------------------
