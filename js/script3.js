44c7f9372159f5f9bcf3a43404e03d6beba3e27f
joannalew <Eggo@Joannas-MBP.lan>
2017-04-17 13:48:48 -0700

    timmy update + lairs

---
+++ js/script3.js
@@ -0,0 +1,468 @@
+'use strict';
+/*
+ *
+ * 1. Variables
+ * 2. General
+ * 3. Lightbox
+ * 4. Portfolio
+ * 5. Map
+ *
+ * */
+
+
+$(document).ready(function () {
+
+    // Variables
+
+    var $window = $(window),
+        $body = $('body'),
+        $loaderBg = $('.loader-bg'),
+        $count_number = $('.count-number'),
+        $timer = $('.timer'),
+        $portfolio = $('#Portfolio'),
+        $filters = $('#filters'),
+        $filtersLink = $('#filters a');
+
+    // -----------------------------------------------------------------------------------------------------
+    // Loader
+
+    setTimeout(function() {
+        $loaderBg.addClass('fadeOut animated');
+        setTimeout(function() {
+            $loaderBg.addClass('hide');
+        }, 500);
+    }, 2500);
+
+
+    $('.open_page').on('click', function () {
+
+        $('.wrapper').addClass('on');
+
+        $('.header').css('background', '#3b434a');
+
+        var $target = $(this).attr('data-target');
+        $('.page').addClass('fadeOutRight animated');
+
+        setTimeout(function () {
+            $('.page').addClass('hide');
+            $('#'+ $target).removeClass('hide fadeOutRight fadeInUp animated').addClass('fadeInUp animated on');
+
+            if ($target == 'about') {
+                setTimeout(function () {
+                    $('.progress .progress-bar').css("width",
+                        function () {
+                            return $(this).attr("aria-valuenow") + "%";
+                        }
+                    );
+
+                    // start all the timers
+                    $timer.each(count);
+                }, 1200);
+            }
+
+            $('.navbar-collapse').removeClass('in');
+        }, 800);
+    });
+
+    $('.open_home').on('click', function () {
+        $('.wrapper').removeClass('on');
+        $('.header').css('background', 'none');
+    });
+
+    setTimeout(function () {
+        $('#projects').addClass('hide');
+    }, 200);
+
+    setTimeout(function () {
+        $('#contact').addClass('hide');
+    }, 1000);
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:1
+    });
+
+
+    // -----------------------------------------------------------------------------------------------------
+    // Lightbox
+    $('.lightbox-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-with-zoom',
+        fixedContentPos: true,
+        closeBtnInside: true,
+
+        zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+
+            duration: 300, // duration of the effect, in milliseconds
+            easing: 'ease-in-out', // CSS transition easing function
+
+            // The "opener" function should return the element from which popup will be zoomed in
+            // and to which popup will be scaled down
+            // By defailt it looks for an image tag:
+            opener: function (openerElement) {
+                // openerElement is the element on which popup was initialized, in this case its <a> tag
+                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        },
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        }
+    });
+
+
+    // -----------------------------------------------------------------------------------------------------
+    // PORTOFOLIO
+
+    $portfolio.isotope({
+        itemSelector: '.portfolio-item',
+        percentPosition: true,
+        masonry: {
+            columnWidth: '.portfolio-sizer'
+        }
+    });
+
+    $filters.on('click', 'a', function () {
+        var filterValue = $(this).attr('data-filter');
+        $filtersLink.removeClass('active');
+        $(this).addClass('active');
+        $portfolio.isotope({
+            filter: filterValue
+        });
+    });
+
+    // -----------------------------------------------------------------------------------------------------
+    // Map
+
+    //set your google maps parameters
+    var latitude = 34.0710288,
+        longitude = -118.0535659,
+        map_zoom = 14;
+
+    //google map custom marker icon - .png fallback for IE11
+    var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+    var marker_url = ( is_internetExplorer11 ) ? 'img/cd-icon-location.png' : 'img/cd-icon-location.svg';
+
+    //define the basic color of your map, plus a value for saturation and brightness
+    var	main_color = '#5B8EFB',
+        saturation_value = -60,
+        brightness_value = 0;
+
+    //we define here the style of the map
+    var style= [
+        {
+            //set saturation for the labels on the map
+            elementType: "labels",
+            stylers: [
+                {saturation: saturation_value}
+            ]
+        },
+        {	//poi stands for point of interest - don't show these lables on the map
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+        {
+            //don't show highways lables on the map
+            featureType: 'road.highway',
+            elementType: 'labels',
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+        {
+            //don't show local road lables on the map
+            featureType: "road.local",
+            elementType: "labels.icon",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+        {
+            //don't show arterial road lables on the map
+            featureType: "road.arterial",
+            elementType: "labels.icon",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+        {
+            //don't show road lables on the map
+            featureType: "road",
+            elementType: "geometry.stroke",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+        //style different elements on the map
+        {
+            featureType: "transit",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "poi",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "poi.government",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "poi.sport_complex",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "poi.attraction",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "poi.business",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "transit",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "transit.station",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "landscape",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+
+        },
+        {
+            featureType: "road",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        },
+        {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [
+                { hue: main_color },
+                { visibility: "on" },
+                { lightness: brightness_value },
+                { saturation: saturation_value }
+            ]
+        }
+    ];
+
+    //set google map options
+    var map_options = {
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: map_zoom,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        styles: style
+    };
+    //inizialize the map
+    var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+    //add a custom marker to the map
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latitude, longitude),
+        map: map
+    });
+
+    //add custom buttons for the zoom-in/zoom-out on the map
+    function CustomZoomControl(controlDiv, map) {
+        //grap the zoom elements from the DOM and insert them in the map
+        var controlUIzoomIn= document.getElementById('cd-zoom-in'),
+            controlUIzoomOut= document.getElementById('cd-zoom-out');
+        controlDiv.appendChild(controlUIzoomIn);
+        controlDiv.appendChild(controlUIzoomOut);
+
+        // Setup the click event listeners and zoom-in or out according to the clicked element
+        google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
+            map.setZoom(map.getZoom()+1)
+        });
+        google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
+            map.setZoom(map.getZoom()-1)
+        });
+    }
+
+    var zoomControlDiv = document.createElement('div');
+    var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+
+    //insert the zoom div on the top left of the map
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+
+
+    // -----------------------------------------------------------------------------------------------------
+    // COUNTER
+
+    $.fn.countTo = function (options) {
+        options = options || {};
+
+        return $(this).each(function () {
+            // set options for current element
+            var settings = $.extend({}, $.fn.countTo.defaults, {
+                from: $(this).data('from'),
+                to: $(this).data('to'),
+                speed: $(this).data('speed'),
+                refreshInterval: $(this).data('refresh-interval'),
+                decimals: $(this).data('decimals')
+            }, options);
+
+            // how many times to update the value, and how much to increment the value on each update
+            var loops = Math.ceil(settings.speed / settings.refreshInterval),
+                increment = (settings.to - settings.from) / loops;
+
+            // references & variables that will change with each update
+            var self = this,
+                $self = $(this),
+                loopCount = 0,
+                value = settings.from,
+                data = $self.data('countTo') || {};
+
+            $self.data('countTo', data);
+
+            // if an existing interval can be found, clear it first
+            if (data.interval) {
+                clearInterval(data.interval);
+            }
+            data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+            // initialize the element with the starting value
+            render(value);
+
+            function updateTimer() {
+                value += increment;
+                loopCount++;
+
+                render(value);
+
+                if (typeof(settings.onUpdate) == 'function') {
+                    settings.onUpdate.call(self, value);
+                }
+
+                if (loopCount >= loops) {
+                    // remove the interval
+                    $self.removeData('countTo');
+                    clearInterval(data.interval);
+                    value = settings.to;
+
+                    if (typeof(settings.onComplete) == 'function') {
+                        settings.onComplete.call(self, value);
+                    }
+                }
+            }
+
+            function render(value) {
+                var formattedValue = settings.formatter.call(self, value, settings);
+                $self.html(formattedValue);
+            }
+        });
+    };
+    $.fn.countTo.defaults = {
+        from: 0, // the number the element should start at
+        to: 0, // the number the element should end at
+        speed: 1000, // how long it should take to count between the target numbers
+        refreshInterval: 100, // how often the element should be updated
+        decimals: 0, // the number of decimal places to show
+        formatter: formatter, // handler for formatting the value before rendering
+        onUpdate: null, // callback method for every time the element is updated
+        onComplete: null // callback method for when the element finishes updating
+    };
+    function formatter(value, settings) {
+        return value.toFixed(settings.decimals);
+    }
+
+    // custom formatting example
+    $count_number.data('countToOptions', {
+        formatter: function (value, options) {
+            return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+        }
+    });
+
+    // // start all the timers
+    // $timer.each(count);
+
+    function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+
+});
